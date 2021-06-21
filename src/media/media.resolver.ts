121b175ce6346075122a55e0dfe3af73d87eecc3
Dan Cristafovici <Dan.Cristafovici@endava.com>
2021-06-21 08:18:43 +0300

    change media resolver, before fix problem

--- src/media/media.resolver.ts
+++ src/media/media.resolver.ts
@@ -2,19 +2,31 @@ import { Resolver, Args, Mutation } from '@nestjs/graphql';
 import { Media } from '../media/media.entity';
 import { GraphQLUpload } from 'apollo-server-express';
 import { createWriteStream } from 'fs';
+import { resolve } from 'path/posix';
 
 @Resolver(() => Media)
 export class MediaResolver {
   @Mutation(() => Boolean)
   async uploadFile(
     @Args({ name: 'file', type: () => GraphQLUpload })
-    { createReadStream, filename },
+    file,
   ): Promise<boolean> {
-    return new Promise(async (resolve, reject) =>
-      createReadStream()
-        .pipe(createWriteStream(`./uploads/${filename}`))
-        .on('finish', () => resolve(true))
-        .on('error', () => reject(false)),
+    await Promise.all(
+      file.map(async (image) => {
+        const singleImage = await image;
+        const { createReadStream, filename } = singleImage;
+        createReadStream()
+          .pipe(createWriteStream(`./uploads/${filename}`))
+          .on('finish', () => console.log('true'))
+          .on('error', () => console.log('false'));
+      }),
     );
+    return true;
+    // return new Promise(async (resolve, reject) =>
+    //   createReadStream()
+    //     .pipe(createWriteStream(`./uploads/${filename}`))
+    //     .on('finish', () => resolve(true))
+    //     .on('error', () => reject(false)),
+    // );
   }
 }
