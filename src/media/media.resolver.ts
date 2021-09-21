f2ff588e5a70ac934b91a8a72c6cd467b8815119
Dan Cristafovici <Dan.Cristafovici@endava.com>
2021-09-21 14:55:01 +0300

    Finlay Media

--- src/media/media.resolver.ts
+++ src/media/media.resolver.ts
@@ -1,4 +1,4 @@
-import { Resolver, Args, Mutation } from '@nestjs/graphql';
+import { Resolver, Args, Mutation, Query } from '@nestjs/graphql';
 import { GraphQLUpload } from 'graphql-upload';
 import { Media } from './media.entity';
 import { MediaService } from './media.service';
@@ -6,6 +6,11 @@ import { MediaService } from './media.service';
 @Resolver(() => Media)
 export class MediaResolver {
   constructor(private mediaService: MediaService) {}
+
+  @Query(() => [Media])
+  async GetImages(): Promise<Media[]> {
+    return await this.mediaService.GetImages();
+  }
 
   @Mutation(() => Boolean)
   async UploadFiles(
--- src/media/media.service.ts
+++ src/media/media.service.ts
@@ -13,24 +13,33 @@ export class MediaService {
     private MediaRepository: Repository<Media>,
   ) {}
 
+  async GetImages(): Promise<Media[]> {
+    return this.MediaRepository.find();
+  }
+
   async UploadFiles(data: MediaDTO): Promise<boolean> {
     const upload = await Promise.all(
       await data.map(async (image) => {
         const singleImage = await image;
         const { createReadStream, filename } = singleImage;
-        const simpleFileName = filename.split('.')[0];
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const simpleFileName = `media_${uniqueSuffix}`;
         const extension = filename.split('.')[1];
         return new Promise(async (resolve, reject) =>
           createReadStream()
-            .pipe(createWriteStream(`./uploads/${filename}`, { flags: 'w+' }))
+            .pipe(
+              createWriteStream(`./uploads/${simpleFileName}.${extension}`, {
+                flags: 'w+',
+              }),
+            )
             .on('finish', async () => {
               const createdItem = await this.MediaRepository.save({
                 name: simpleFileName,
-                path: `./uploads/${filename}`,
+                path: `./uploads/${simpleFileName}.${extension}`,
               });
               MediaSizes.map(
                 async (size) =>
-                  await sharp(`./uploads/${filename}`)
+                  await sharp(`./uploads/${simpleFileName}.${extension}`)
                     .resize({ width: size.x })
                     .toFile(
                       `./uploads/build/${simpleFileName}_${size.path}.${extension}`,
