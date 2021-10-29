36fcaa1fe812d4a1f6e6b6c0454662f4a373c30e
Dan Cristafovici <Dan.Cristafovici@endava.com>
2021-10-29 08:38:59 +0300

    fix

--- src/code/code.service.ts
+++ src/code/code.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AddCodeInput } from './code.dto';
 import { Code } from './code.entity';
+import axios from 'axios';
 
 @Injectable()
 export class CodeService {
@@ -13,7 +14,22 @@ export class CodeService {
 
   async sendPhoneAndGetStatus(phone: string): Promise<boolean> {
     const code = Math.floor(1000 + Math.random() * 9000);
+    await axios
+      .post(
+        `https://sms.ru/sms/send?api_id=92A71F47-3172-BFCB-208C-9C025611EC78&to=${phone}&msg=${code}&json=1`,
+      )
+      .then(async ({ data }) => {
+        if (data.status_code === 100) {
+          await this.CodeRepository.save({ code, phone });
+          return true;
+        }
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
+
     await this.CodeRepository.save({ code, phone });
+
     return true;
   }
 
@@ -24,6 +40,11 @@ export class CodeService {
       order: { created_at: 'DESC' },
     });
     const check = item.code === code;
-    return check ? true : false;
+
+    if (!check) {
+      throw new HttpException('Code is wrong. Try again', 403);
+    }
+
+    return check;
   }
 }
